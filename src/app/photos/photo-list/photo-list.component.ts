988d92f66acdd8ae2eba723f87eadd8f29e6a67a
Maycon <mayconsalheb@hotmail.com>
2018-09-13 00:34:11 -0300

    Added debounce pattern in input

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { PhotoService } from "../photo/Photo.service";
 import { ActivatedRoute } from "@angular/router";
+import { Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators"
 
 @Component({
     selector    : "ap-photo-list",
@@ -10,6 +11,7 @@ export class PhotoListComponent implements OnInit{
 
     photos: Object[] = [];
     filter: string = '';
+    debounce: Subject<string> = new Subject<string>();
     
     constructor(
         private activatedRoute: ActivatedRoute
@@ -17,5 +19,8 @@ export class PhotoListComponent implements OnInit{
     
     ngOnInit(): void {
         this.photos = this.activatedRoute.snapshot.data.photos; 
+        this.debounce
+                .pipe(debounceTime(300))
+                .subscribe(filter => this.filter = filter);
     }
 }
